8d00c63f6cc363155210470448cfefd14a27871d
krishnadev <krishnadevkichus@gmail.com>
2024-12-23 15:48:40 +0530

    fix import path for Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero } from '@/components/hero'
+import { Hero } from '@/components/Hero'
 import FeaturedCategories from '@/components/featured-categories'
 import PopularProducts from '@/components/popular-products'
 import SellGames from '@/components/sell-games'
